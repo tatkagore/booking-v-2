d806dff17e9534d3bdca343e59e1f57de0b3efb9
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2023-09-26 10:37:45 +0200

    Add models

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,23 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Reservation = require("../models/reservation");
 
-/* GET User */
-router.get('/', function(req, res, next) {
+/* GET Reservation */
+router.get("/", function (req, res, next) {
     res.json({ message: "Hello, get Reservation!" });
 });
 
-/* Post User */
-router.post('/', function(req, res, next) {
-    res.json({ message: "Hello, post Reservation!" });
+/* Post Reservation */
+router.post("/", async (req, res, next) => {
+    const reservation = await Reservation.create({
+    date: "12",
+    name: "Tanya",
+    note: "Good",
+    status: "1",
+    userId: 1,
+    spotId: 1,
+    roomId: 1,
 });
 
-/* Put User. */
-router.put('/', function(req, res, next) {
+    res.json({ message: reservation });
+});
+
+/* Put Reservation. */
+router.put("/", function (req, res, next) {
     res.json({ message: "Hello, put Reservation!" });
 });
 
-/* Delete User */
-router.delete('/', function(req, res, next) {
+/* Delete Reservation */
+router.delete("/", function (req, res, next) {
     res.json({ message: "Hello, delete Reservation!" });
 });
 
