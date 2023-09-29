ea1fa24b51f7373eff53d816a2d9571f91bd3d0d
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2023-09-29 11:35:02 +0200

    fix: changed func to restrict creating account with the same email

--- routes/auth.js
+++ routes/auth.js
@@ -1,48 +1,69 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const { User } = require('../db');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const { User } = require("../db");
+const jwt = require("jsonwebtoken");
 
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
+  try {
+    // Check if the email already exists in the database
+    const existingUser = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    // If an existing user is found, respond with an error
+    if (existingUser) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+
     // if req.body.password is empty, add "Password field is empty"
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const { role, firstName, lastName, email, phoneNumber } = req.body
-    const user = { 
-        role,
-        firstName,
-        lastName,
-        email,
-        phoneNumber,
-        password: hashedPassword 
+    const { role, firstName, lastName, email, phoneNumber } = req.body;
+    const user = {
+      role,
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      password: hashedPassword,
     };
     await User.create(user);
-    res.json({ message: 'User created', user });
+    res.json({ message: "User created", user });
+  } catch (error) {
+    // Handle any errors that occur during user creation
+    next(error);
+  }
 });
 
-const SECRET_KEY = 'secretkey23456'; // A remplacer par une clé secrète
+const SECRET_KEY = "secretkey23456"; // A remplacer par une clé secrète
 
-router.post('/signin', async (req, res) => {
-const user = await User.findOne({
+router.post("/signin", async (req, res) => {
+  const user = await User.findOne({
     where: {
-        email: req.body.email,
+      email: req.body.email,
     },
-});
+  });
 
-    if (!user) return res.status(400).json({message: `Nom d'utilisateur ou mot de passe incorrect`});
+  if (!user)
+    return res
+      .status(400)
+      .json({ message: `Nom d'utilisateur ou mot de passe incorrect` });
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).json({message: `Nom d'utilisateur ou mot de passe incorrect`});
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword)
+    return res
+      .status(400)
+      .json({ message: `Nom d'utilisateur ou mot de passe incorrect` });
 
-    const payload = {
-        email: req.body.email,
-        id: user.id
+  const payload = {
+    email: req.body.email,
+    id: user.id,
 
     // Vous pouvez ajouter d'autres propriétés ici
-    };
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
-    res.json({message: token});
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+  res.json({ message: token });
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,27 +3,27 @@ var router = express.Router();
 const { Sequelize, DataTypes } = require("sequelize");
 const config = require("../config/config.json")["development"];
 const sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    {
+  config.database,
+  config.username,
+  config.password,
+  {
     host: config.host,
     dialect: config.dialect,
-    }
+  }
 );
 const { User } = require("../db.js");
 
-/* GET User */
+/* GET Users */
 router.get("/", async (req, res, next) => {
-    // Retrieve all users from the database
-    const users = await User.findAll();
-    res.json(users);
+  // Retrieve all users from the database
+  const users = await User.findAll();
+  res.json(users);
 });
 
-/* GET currentuser */
-router.get('/currentUser', async (req, res, next) => {
-    try {
-    // Get the user ID 
+/* GET One User */
+router.get("/currentUser", async (req, res, next) => {
+  try {
+    // Get the user ID
     const userId = req.user.id;
 
     // Find the user by ID
@@ -31,45 +31,66 @@ router.get('/currentUser', async (req, res, next) => {
 
     // If the user is not found, send a 404 error
     if (!currentUser) {
-        return res.status(404).json({ error: 'user not found' });
+      return res.status(404).json({ error: "user not found" });
     }
 
     // Return the user information
     res.status(200).json({ currentUser });
-    } catch (error) {
+  } catch (error) {
     // If there is an error, send the error message to the client
     next(error);
-    }
+  }
+});
+
+/* GET user */
+router.get("/me", (req, res, next) => {
+  User.findOne({ where: { id: req.body.userId } }).then((users) => {
+    res.status(200).send(users);
+  });
 });
 
 /* Post User */
 router.post("/", async (req, res, next) => {
+  try {
+    // Check if the email already exists in the database
+    const existingUser = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    // If an existing user is found, respond with an error
+    if (existingUser) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+
     const user = await User.create({
-    role: "Guest",
-    firstName: "Tanya",
-    lastName: "Simmer",
-    email: "tanya@gmail.com",
-    phoneNumber: "555-555",
-    password: "password",
-});
-    res.json({ message: user});
+      role: "Guest",
+      firstName: "Tanya",
+      lastName: "Simmer",
+      email: "tanya@gmail.com",
+      phoneNumber: "555-555",
+      password: "password",
+    });
+    res.json({ message: user });
+  } catch (error) {
+    // Handle any errors that occur during user creation
+    next(error);
+  }
 });
 
 /* Put User. */
-router.put('/', async (req, res, next) => {
-    const user = await User.findByPk(1);
-    user.firstName = "Thomas"
-    user.save()
-    res.json({ message: "Hello, put Room!" });
+router.put("/", async (req, res, next) => {
+  const user = await User.findByPk(1);
+  user.firstName = "Thomas";
+  user.save();
+  res.json({ message: "Hello, put Room!" });
 });
-
 
 /* Delete User */
 
-router.delete("/", async  (req, res, next) => {
-    const user = await User.findByPk(1);
-    await user.destroy();
-    res.json({ message: "Hello, delete Reservation!" });
+router.delete("/", async (req, res, next) => {
+  const user = await User.findByPk(1);
+  await user.destroy();
+  res.json({ message: "Hello, delete Reservation!" });
 });
 
 module.exports = router;
