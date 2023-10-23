ee4d7eac2853d5a92b61c27a278504654668ce63
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2023-10-23 19:50:18 +0200

    fix: deleted comments

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const { User } = require("../db");
 const jwt = require("jsonwebtoken");
+const SECRET_KEY = "secretkey23456"; 
+
 
 // Regular expression for email validation
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -23,9 +25,6 @@ router.post("/signup", async (req, res) => {
       return res.status(400).json({ error: "Invalid email format" });
     }
 
-    // if req.body.password is empty, add "Password field is empty"
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
     const { role, firstName, lastName, email, phoneNumber } = req.body;
     const user = {
       role,
@@ -38,12 +37,9 @@ router.post("/signup", async (req, res) => {
     await User.create(user);
     res.json({ message: "User created", user });
   } catch (error) {
-    // Handle any errors that occur during user creation
     next(error);
   }
 });
-
-const SECRET_KEY = "secretkey23456"; // A remplacer par une clé secrète
 
 router.post("/signin", async (req, res) => {
   const user = await User.findOne({
@@ -66,8 +62,6 @@ router.post("/signin", async (req, res) => {
   const payload = {
     email: req.body.email,
     id: user.id,
-
-    // Vous pouvez ajouter d'autres propriétés ici
   };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
   res.json({ message: token });
