49b8b888aecef612b775d506406474533db78ddc
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2023-10-27 12:34:06 +0200

    feat: aded cors package

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const authRouter = require("./routes/auth");
 const jwt = require("jsonwebtoken");
 const { verifyJWT } = require("./middlewares.js");
+const cors = require("cors");
 
 // Middlewares (code executed between req and res)
 app.use(logger("dev"));
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors({ origin: "http://localhost:8000" }));
 
 // app implements a router plus middlewares (logger, json, url encoded, cookie parser, etc.)
 app.use("/api", verifyJWT, indexRouter); // All routes check verifyJwt but auth
